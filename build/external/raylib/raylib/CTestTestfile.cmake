41be2dff9f30a7bf30928e22b0014ea1cde5fb6c
xTarzx <telmomartins2000@gmail.com>
2024-11-14 20:21:24 +0000

    cmake shit

---
+++ build/external/raylib/raylib/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: /home/xtarzx/RandomCoding/CPP/mortal/external/raylib/src
+# Build directory: /home/xtarzx/RandomCoding/CPP/mortal/build/external/raylib/raylib
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("external/glfw")
---
+++ build/src/CMakeFiles/mortal.dir/cmake_clean.cmake
@@ -0,0 +1,18 @@
+file(REMOVE_RECURSE
+  "../bin/mortal"
+  "../bin/mortal.pdb"
+  "CMakeFiles/mortal.dir/link.d"
+  "CMakeFiles/mortal.dir/main.cpp.o"
+  "CMakeFiles/mortal.dir/main.cpp.o.d"
+  "CMakeFiles/mortal.dir/pose.cpp.o"
+  "CMakeFiles/mortal.dir/pose.cpp.o.d"
+  "CMakeFiles/mortal.dir/psik.cpp.o"
+  "CMakeFiles/mortal.dir/psik.cpp.o.d"
+  "CMakeFiles/mortal.dir/serialize.cpp.o"
+  "CMakeFiles/mortal.dir/serialize.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/mortal.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
