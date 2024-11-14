e931f8be653b18bb896b96e59e69dc99e8ed6425
xTarzx <telmomartins2000@gmail.com>
2024-11-14 20:48:43 +0000

    rm build

--- build/external/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/xtarzx/RandomCoding/CPP/mortal/external/raylib
-# Build directory: /home/xtarzx/RandomCoding/CPP/mortal/build/external/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
