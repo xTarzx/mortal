a24ebf70367482dcf26b24d3393ef0b292b4ce3a
xTarzx <telmomartins2000@gmail.com>
2024-11-17 01:56:42 +0000

    make bro more flexible

--- include/common.h
+++ include/common.h
@@ -5,11 +5,11 @@ const float PPM = 100;
 #define HIP_LOWER_ANGLE (-b2_pi * 0.25f)
 #define HIP_UPPER_ANGLE (b2_pi * 0.72f)
 
-#define KNEE_LOWER_ANGLE (-b2_pi * 0.6f)
+#define KNEE_LOWER_ANGLE (-b2_pi * 0.8f)
 #define KNEE_UPPER_ANGLE 0.0f
 
 #define ANKLE_LOWER_ANGLE (-b2_pi * 0.35f)
-#define ANKLE_UPPER_ANGLE (b2_pi * 0.07f)
+#define ANKLE_UPPER_ANGLE (b2_pi * 0.27f)
 
 #define SHOULDER_LOWER_ANGLE (-b2_pi * 0.38f)
 #define SHOULDER_UPPER_ANGLE (b2_pi * 0.99f)
